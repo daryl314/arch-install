bb2a86bcfc6d52b0192e62cfb472a2415b18268a
Daryl St. Laurent <daryl.stlaurent@gmail.com>
2013-04-19 13:43:48 -0400

    Improvements to partitioning

--- install.sh
+++ install.sh
@@ -3,13 +3,19 @@
 # terminate script on errors (-ex for trace too)
 set -e
 
-# display partition table
-parted -l
+# -------------------------------------------------- 
+# Partitioning
+# -------------------------------------------------- 
 
-# capture defaults
-DRIVE=`parted -l | grep -Po '/dev/sd\w'`
-ROOT=`parted -l | grep ext4 | head -n 1 | cut -c 2`
-HOME=`parted -l | grep ext4 | tail -n 1 | cut -c 2`
+# display partition table(s)
+lsblk | grep '^sd' | cut -c 1-3 | xargs -i parted /dev/{} print
+
+# assume first sdx drive for now
+DRIVE=/dev/`lsblk | grep '^sd' | head -n 1 | cut -c 1-3`
+
+# assume first ext4 partition is root, last ext4 partition is home
+ROOT=`parted "$DRIVE" print | grep ext4 | head -n 1 | cut -c 2`
+HOME=`parted "$DRIVE" print | grep ext4 | tail -n 1 | cut -c 2`
 
 # prompt for root and home partitions
 read -e -p "Root partition: " -i "$DRIVE$ROOT" ROOT
